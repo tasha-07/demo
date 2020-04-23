22b8a08e0aa11589ed60d473ce79c46e4769649c
Tarun Sharma <46851510+amrahs-nurat@users.noreply.github.com>
2020-04-23 14:17:30 +0530

    script

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Setup a proper path, I call my virtualenv dir "venv" and
+# I've got the virtualenv command installed in /usr/local/bin
+PATH=$WORKSPACE/venv/bin:/usr/local/bin:$PATH
+if [ ! -d "venv" ]; then
+        virtualenv venv
+fi
+. venv/bin/activate
+pip install -r requirements.txt --download-cache=/tmp/$JOB_NAME
